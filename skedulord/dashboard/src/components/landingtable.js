6ef87321eeb53d7478b68d1e75eaca08775ce7f6
vincent <vincentwarmerdam@gmail.com>
2019-06-18 23:28:19 +0200

    even moar progress!

--- skedulord/dashboard/src/components/landingtable.js
+++ skedulord/dashboard/src/components/landingtable.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Button from '@material-ui/core/Button';
 import JobPanel from "../components/jobpanel.js"
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Button from '@material-ui/core/Button';
 
 class App extends React.Component {
   constructor() {
@@ -27,18 +27,16 @@ class App extends React.Component {
       const commands = jobs
                         .map(row => row.command)
                         .reduce((acc, d) => acc.includes(d) ? acc : acc.concat(d), []);
-      console.log(jobs);
-      console.log(commands);
       return (
         <div>
-            {commands
+            {jobs
               .map(job => (
-              <ExpansionPanel key={job}>
+              <ExpansionPanel key={job.id}>
               <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content" key={job.id}>
-                <Typography key={job.id}><pre><b>{job}</b></pre></Typography>
+                aria-controls="panel1a-content" key={job.command}>
+                <Typography key={job.id}><pre><b>{job.command}</b></pre></Typography>
               </ExpansionPanelSummary>
-              <JobPanel></JobPanel>
+              <JobPanel key={job.id} jobs={job.jobs}></JobPanel>
               </ExpansionPanel>
             ))}
         </div>
--- skedulord/dashboard/src/pages/index.js
+++ skedulord/dashboard/src/pages/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import LandingTable from '../components/landingtable.js'
+import Logo from '../components/logo.js'
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
@@ -36,11 +28,12 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function PermanentDrawerLeft() {
+export default function App() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
+        <Logo></Logo>
         <LandingTable></LandingTable>
     </div>
   );
