838fea6bf654e5f5ae0ab160d7a7fa0f1e7778fb
vincent <vincentwarmerdam@gmail.com>
2019-10-04 22:49:40 +0200

    another dep

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-base_packages = ["Click==7.0", "flask", "flask_cors"]
+base_packages = ["Click==7.0", "flask", "flask_cors", "waitress"]
 
 dev_packages = ["pip", "pytest-cov", "pytest", "flake8", "mkdocs", "mkdocs-material"]
 
